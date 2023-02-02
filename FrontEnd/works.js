3771c7a2e08ec7a3e122f82945eba11505a77601
julien33510 <120409256+julien33510@users.noreply.github.com>
2023-02-02 17:27:02 +0100

    Affichage des travaux depuis backend - Etape 1.1

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -3,6 +3,24 @@
 const reponse = await fetch ("http://localhost:5678/api/works");
 const repnoseWorks = await reponse.json();
 
-/*console.log (repnoseWorks); */
+console.log (repnoseWorks);
 
 //Affichage des travaux
+function worksWiew(works) {
+    for (let i=0 ; i < works.length ; i++ ) {
+        const workElement = document.createElement("figure");
+        const workImg = document.createElement("img");
+        workImg.crossOrigin = "anonymous";
+        workImg.src = works[i].imageUrl;
+        workImg.title = works[i].title;
+        const workFigcaption = document.createElement("figcaption");
+        workFigcaption.innerText = works[i].title;
+        workElement.appendChild(workImg);
+        workElement.appendChild(workFigcaption);
+
+        document.querySelector(".gallery").appendChild(workElement);
+    }
+}
+
+worksWiew(repnoseWorks);
+
