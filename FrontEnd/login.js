66e58abdaa5eda18ced28276364a8e3d720af66a
julien33510 <120409256+julien33510@users.noreply.github.com>
2023-02-10 09:55:41 +0100

    Page login fonctionnelle
    login.html, fetch, traitement des erreurs, redirection sur index et fonction logout

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,4 +1,94 @@
-export function login () {
-    
-    
-}
+//au click sur le bouton se connecter
+
+    const boutonSeConnecter = document.querySelector("#boutonSeConnecter");
+    boutonSeConnecter.addEventListener("click", async function(event){
+        event.preventDefault();
+        let emailConnection = document.querySelector("#email").value;
+        let passConnection = document.querySelector("#pass").value;
+        
+        let user = {
+            "email": emailConnection,
+            "password": passConnection
+        };
+
+        try {
+            let reponseConnect = await fetch('http://localhost:5678/api/users/login',{
+                method:'POST',
+                headers:{
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(user)
+            });
+
+            if (reponseConnect.status == 200) {
+                console.log ("200");
+                reponseConnect = await reponseConnect.json();
+                let token = reponseConnect.token;
+                console.log(token);
+                window.localStorage.setItem("tokenSession", token);
+                document.location.href="index.html"; 
+            } else if (reponseConnect.status == 401) {
+                // Mauvais mot de passe
+
+                //On remet les label "Email" et "Mot de passe" comme au départ
+                const userNotFound = document.querySelector("form div.divChamp label[for='email']");
+                userNotFound.innerHTML = "E-mail";
+                const userNotAuthorized = document.querySelector("form div.divChamp label[for='pass']");
+                userNotAuthorized.innerHTML = "Mot de passe";
+                // on indique l'erreur sur l'email
+                let span = document.createElement("span");
+                span.classList.add("error");
+                span.innerText = " - Mot de passe incorrect";
+                userNotAuthorized.appendChild(span);
+            } else {
+                //Utilisateur inconnu - Reponse 404
+
+                 //On remet les label "Email" et "Mot de passe" comme au départ
+                 const userNotFound = document.querySelector("form div.divChamp label[for='email']");
+                 userNotFound.innerHTML = "E-mail";
+                 const userNotAuthorized = document.querySelector("form div.divChamp label[for='pass']");
+                 userNotAuthorized.innerHTML = "Mot de passe";
+                // on indique l'erreur sur le user
+                userNotFound.innerHTML = "E-mail"; // Si on ne le fait pas à chaque essai on ajoute le txt d'erreur
+                let span = document.createElement("span");
+                span.classList.add("error");
+                span.innerText = " - Utilisateur inconnu";
+                userNotFound.appendChild(span);
+            } } catch (e) {
+                const errorDiv = document.querySelector("section.SectionLogin");
+                let errorAlert = document.createElement("div");
+                errorAlert.classList.add("error");
+                errorAlert.innerText = "Problème de connexion serveur";
+                errorDiv.appendChild(errorAlert);
+            }
+
+        
+        
+        //location.href='index.html';
+
+    });
+
+//Affichage du password
+
+    const boutonEye = document.querySelector(".field-icon");
+    boutonEye.addEventListener("click",function(){
+        
+        let inputField = document.querySelector("div.divChamp input#pass");
+        let inputType = inputField.getAttribute("type");
+        let iconEye = document.querySelector("div.divChamp span.field-icon i");
+        
+        if (inputType == "password") {
+            inputField.removeAttribute("type");
+            inputField.setAttribute("type","text");
+            iconEye.classList.remove("fa-eye");
+            iconEye.classList.add("fa-eye-slash");
+        } else {
+            inputField.removeAttribute("type");
+            inputField.setAttribute("type","password");
+            iconEye.classList.remove("fa-eye-slash");
+            iconEye.classList.add("fa-eye");
+        };
+    })
+
+// document.location.href="http://manouvellepage.com"; 
+
--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,76 +1,96 @@
-//import
-import {login} from "./login.js";
 
 
 // Récupération des travaux depuis le backend
-const reponse = await fetch ("http://localhost:5678/api/works");
-const reponseWorks = await reponse.json();
+    const reponse = await fetch ("http://localhost:5678/api/works");
+    const reponseWorks = await reponse.json();
 
-console.log (reponseWorks);
 
 //Affichage des travaux
-function worksWiew(works) {
-    document.querySelector(".gallery").innerHTML = '';
-    for (let i=0 ; i < works.length ; i++ ) {
-        const workElement = document.createElement("figure");
-        const workImg = document.createElement("img");
-        workImg.crossOrigin = "anonymous";
-        workImg.src = works[i].imageUrl;
-        workImg.title = works[i].title;
-        const workFigcaption = document.createElement("figcaption");
-        workFigcaption.innerText = works[i].title;
-        workElement.appendChild(workImg);
-        workElement.appendChild(workFigcaption);
+    function worksWiew(works) {
+        document.querySelector(".gallery").innerHTML = '';
+        for (let i=0 ; i < works.length ; i++ ) {
+            const workElement = document.createElement("figure");
+            const workImg = document.createElement("img");
+            workImg.crossOrigin = "anonymous";
+            workImg.src = works[i].imageUrl;
+            workImg.title = works[i].title;
+            const workFigcaption = document.createElement("figcaption");
+            workFigcaption.innerText = works[i].title;
+            workElement.appendChild(workImg);
+            workElement.appendChild(workFigcaption);
 
-        document.querySelector(".gallery").appendChild(workElement);
+            document.querySelector(".gallery").appendChild(workElement);
+        }
     }
-}
 
-worksWiew(reponseWorks);
+    worksWiew(reponseWorks);
 
 
 //bouton filtres
+    //fonction de suppression de la classe "Activate" dans l'ensemble des boutons "filtre"
+    function supprActivate () {
+        let allChildrenFilter = document.querySelectorAll(".filter");
+        allChildrenFilter.forEach((item)=>item.classList.remove("activate"));
+    }
+    //bouton Filtre => Tous
+    const boutonFiltreTous = document.querySelector("#tous");
+    boutonFiltreTous.addEventListener("click", function(){
+        worksWiew(reponseWorks);
+        supprActivate ();
+        boutonFiltreTous.classList.add("activate");
+    })
+    //bouton Filtre => Objets
+    const boutonFiltreObjet = document.querySelector("#objets");
+    boutonFiltreObjet.addEventListener("click", function(){
+        const reponseWorksObjet = reponseWorks.filter(i => i.categoryId == 1);
+        worksWiew(reponseWorksObjet);
+        supprActivate ();
+        boutonFiltreObjet.classList.add("activate");
+    })
+    //bouton Filtre => appartements
+    const boutonFiltreAppartements = document.querySelector("#appartements");
+    boutonFiltreAppartements.addEventListener("click", function(){
+        const reponseWorksAppartements = reponseWorks.filter(i => i.categoryId == 2);
+        worksWiew(reponseWorksAppartements);
+        supprActivate ();
+        boutonFiltreAppartements.classList.add("activate");
+    })
+    //bouton Filtre => hotels
+    const boutonFiltreHotels = document.querySelector("#hotels");
+    boutonFiltreHotels.addEventListener("click", function(){
+        const reponseWorksHotels = reponseWorks.filter(i => i.categoryId == 3);
+        worksWiew(reponseWorksHotels);
+        supprActivate ();
+        boutonFiltreHotels.classList.add("activate");
+    })
 
-//fonction de suppression de la classe "Activate" dans l'ensemble des boutons "filtre"
-function supprActivate () {
-    let allChildrenFilter = document.querySelectorAll(".filter");
-    allChildrenFilter.forEach((item)=>item.classList.remove("activate"));
-}
 
-//bouton Filtre => Tous
-const boutonFiltreTous = document.querySelector("#tous");
-boutonFiltreTous.addEventListener("click", function(){
-    worksWiew(reponseWorks);
-    supprActivate ();
-    boutonFiltreTous.classList.add("activate");
-})
 
-//bouton Filtre => Objets
-const boutonFiltreObjet = document.querySelector("#objets");
-boutonFiltreObjet.addEventListener("click", function(){
-    const reponseWorksObjet = reponseWorks.filter(i => i.categoryId == 1);
-    worksWiew(reponseWorksObjet);
-    supprActivate ();
-    boutonFiltreObjet.classList.add("activate");
-})
 
-//bouton Filtre => appartements
-const boutonFiltreAppartements = document.querySelector("#appartements");
-boutonFiltreAppartements.addEventListener("click", function(){
-    const reponseWorksAppartements = reponseWorks.filter(i => i.categoryId == 2);
-    worksWiew(reponseWorksAppartements);
-    supprActivate ();
-    boutonFiltreAppartements.classList.add("activate");
-})
+//Gestion du token
 
-//bouton Filtre => hotels
-const boutonFiltreHotels = document.querySelector("#hotels");
-boutonFiltreHotels.addEventListener("click", function(){
-    const reponseWorksHotels = reponseWorks.filter(i => i.categoryId == 3);
-    worksWiew(reponseWorksHotels);
-    supprActivate ();
-    boutonFiltreHotels.classList.add("activate");
-})
+    let token = window.localStorage.getItem("tokenSession");
+    console.log(token);
+
+    if (token !== null) {
+        //modification de la nav login en logout et insertion de la fonction logout
+        console.log('token n\'est pas null');
+        console.log(typeof(token));
+        const loginNavElement = document.querySelector("nav ul li a");
+        loginNavElement.innerHTML = "logout";
+        loginNavElement.removeAttribute("href");
+        loginNavElement.addEventListener("click", function(){
+            localStorage.clear();
+            document.location.href="index.html"; 
+        });
+
+        
+    }
+
+    
+    
+
+//Fonction de logout
 
 
 
